3cb4769db59027ca29dab4029569e3159186f1e9
bunbunjin <bunbunjin.aisu@gmail.com>
2020-09-06 17:12:10 +0900

    scheduleを使って書き直した

--- juken.py
+++ juken.py
@@ -1,14 +1,21 @@
 from time import sleep
+import schedule
 from datetime import datetime
 
 dt = datetime(2020, 10, 1)
+
+
+def job():
+    nokori = dt - now
+    day = nokori.days
+    days = int(day) + 1
+    print(f'本番まで{days}日だZO!!')
+
+
+schedule.every().day.at('00:00').do(job)
+
+
 while True:
-    date = datetime.now()
-    date_str = str(date)
-    tim = date_str[11:-10]
-    if '00:00' == tim:
-        nokori = dt - date
-        day = nokori.days
-        days = int(day) + 1
-        print(f'本番まで{days}日だZO!!')
-        sleep(60)
+    now = datetime.now()
+    schedule.run_pending()
+    sleep(1)
